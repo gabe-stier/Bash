8bf32b7d61b21c580279f8ae0d2b1c8353e07cb6
Gabe Stier <37159135+gabe-stier@users.noreply.github.com>
2019-11-19 18:01:00 -0600

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# A function that askes the users what they would like to take a look at. 
 function ask(){
         echo "What would like to monitor?";
         echo "> 1: Memory"
@@ -7,30 +8,31 @@ function ask(){
         echo "> 0: Exit"
         echo "Please input the number:"
         read ans;
-        return $ans;
 }
+
+# The main function of the code. Interprets what the user has requested
 function monitor(){
 ask
 while true; do
 
         case $ans in
-                1)
+                1) # Is called if the user wants to see the memory of the machine
                         free -m
                         ;;
-                2)
+                2) # Is called if the user wants to see the disk space of the machine
                         df -h
                         ;;
-                3)
+                3) # Is called if the user wants to see the network connections of the machine
                         netstat
                         ;;
-                0)
+                0) # Is called if the user wants to exit the script
                         end=True
                         ;;
-                *)
+                *) # Is called if the number given is not 0, 1, 2, or 3
                         echo 'Invalid Choice'
                         ;;
         esac
-        if [ $end ]
+        if [ $end ] # Checks to see if the scripts is to end.
         then
                 echo "Exiting"
                 break
